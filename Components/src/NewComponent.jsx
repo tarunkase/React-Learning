b89013892243ebbd2524ca0e8594afa464fa8738
Tarun <tarunkase178@gmail.com>
2026-09-22 15:58:21 +0530

    changes in component

--- Components/src/NewComponent.jsx
+++ Components/src/NewComponent.jsx
@@ -4,8 +4,11 @@ import NestedComponent from './assets/NestedComponent';
 export default function NewComponent() {
   return (
     <>
-    <div>NewComponent</div>
+    <div className="card">
+    <h1>NewComponent</h1>
+    <h2>This is New Component file created separately</h2>
     <img src='../assets/hero.png'></img>
+    </div>
     <NestedComponent />
     </>
     
