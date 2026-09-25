66abf0643f26fd1ea0af8ebfd61adb10635b0cf6
Tarun <tarunkase178@gmail.com>
2026-09-25 17:21:47 +0530

    added image

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import CardComponent from './components/CardComponent';
+import id1 from "./assets/ID-PD.jpg";
+import id2 from "./assets/ID-TK.jpg"
+
 
 function App() {
   return (
     <>
      
-      <CardComponent userName="Priyanka Copade" age={24} city="Pune" />
-      <CardComponent userName="Tarun Kase" age={25} city="Kolhapur" />
+      <CardComponent userName="Priyanka Copade" age={24} city="Pune" img={id1}/>
+      <CardComponent userName="Tarun Kase" age={25} city="Kolhapur" img={id2}/>
     </>
   )
 }
--- props/src/components/CardComponent.jsx
+++ props/src/components/CardComponent.jsx
@@ -8,8 +8,8 @@ function CardComponent(props) {
     <>
     <div className="cardParent">
       <div className="card">
-          <img src="https://plus.unsplash.com/premium_photo-1787742235469-86fc784b8981?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
-          <h1>{props.userName}</h1>
+          <img src={props.img}></img>
+          <h1>{props.userName } {props.age}</h1>
           <div>A React Developer with hands-on experience of HTML, CSS, JS, Bootstarp and React.JS</div>
           <ButtonComponent />
           
